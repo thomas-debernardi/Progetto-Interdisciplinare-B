214be24beb0fed78e3a3b086090b440eeae67e08
Thomas16898 <thomasdb59@gmail.com>
2019-11-19 17:45:55 +0100

    6

--- RuotaDellaFortuna/src/Services/Notification.java
+++ RuotaDellaFortuna/src/Services/Notification.java
@@ -1,9 +1,34 @@
 package Services;
 
-import javax.swing.JFrame;
 
-public class Notification extends JFrame{
-	public static void notify(String title, String msg, boolean error) {
-		
-	}
+import java.nio.charset.StandardCharsets;
+
+import GUI.NotificationModel;
+
+public class Notification {
+    /**
+     * @param title il titolo della notifica
+     * @param msg   il messaggio della notifica
+     * @param error <code>true</code> se far visualizzare la notifica come errore, <code>false</code> altrimenti
+     */
+    public static void notify(String title, String msg, boolean error) {
+        byte[] bTitle = title.getBytes();
+        byte[] bMsg = msg.getBytes();
+        int duration = 3;
+        if (error) {
+            NotificationModel.create()
+                    .title(new String(bTitle, StandardCharsets.UTF_8))
+                    .text(new String(bMsg, StandardCharsets.UTF_8))
+                    .hideAfter(Duration.seconds(duration))
+                    .position(Pos.BASELINE_RIGHT)
+                    .showError();
+        } else {
+            NotificationModel.create()
+                    .title(new String(bTitle, StandardCharsets.UTF_8))
+                    .text(new String(bMsg, StandardCharsets.UTF_8))
+                    .hideAfter(Duration.seconds(duration))
+                    .position(Pos.BASELINE_RIGHT)
+                    .showInformation();
+        }
+    }
 }
